abce886dc1c36120319b02eaad12e48536fdd62f
Francois-Regis Jaunatre <frjaunatre@sodiuswillert.com>
2024-09-21 10:46:23 +0200

    add a generated svg favicon

--- index.mjs
+++ index.mjs
@@ -282,6 +282,7 @@ app.get('/', async (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="bitbucket-pr-tree-icon.svg">
     <title>Bitbucket Pull Requests Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <style>
